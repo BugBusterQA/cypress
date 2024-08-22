34706d95e32db6febf51bf8f4a207799792344ca
bugbusterqa <bugbusterqa@rambler.ru>
2024-08-22 17:20:50 +0300

    add tests

--- e2e/spec.cy.js
+++ e2e/spec.cy.js
@@ -2,6 +2,8 @@ describe('template automation', () => {
 
   it('passes', () => {
     cy.visit('https://example.cypress.io')
+    cy.contains('Hello, World')
+    cy.contains('Kitchen Sink')
   })
-  
+
 })
