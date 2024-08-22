38ddf6bd6a3a0a421013e0b183158ac2b0f91779
bugbusterqa <bugbusterqa@rambler.ru>
2024-08-22 17:22:54 +0300

    some changes

--- e2e/spec.cy.js
+++ e2e/spec.cy.js
@@ -2,7 +2,6 @@ describe('template automation', () => {
 
   it('passes', () => {
     cy.visit('https://example.cypress.io')
-    cy.contains('Hello, World')
     cy.contains('Kitchen Sink')
   })
 
